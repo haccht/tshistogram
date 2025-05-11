b26f1c06159266c3db675ffb434764fdd6dcace5
haccht <haccht@users.noreply.github.com>
2025-05-11 16:23:24 +0900

    Refactor

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 	"text/tabwriter"
@@ -15,95 +16,7 @@ import (
 	"golang.org/x/term"
 )
 
-type options struct {
-	TimeLayout   string `short:"f" long:"format" description:"Time format for parsing the input text" default:"RFC3339"`
-	TimeInterval string `short:"i" long:"interval" description:"Time duration for aggregation" default:"5m"`
-	TimeFrom     string `long:"time-from" description:"Time to display the chart from"`
-	TimeTo       string `long:"time-to" description:"Time to display the chart to"`
-	TimeZone     string `long:"time-zone" description:"TimeZone to display the time" default:"UTC"`
-	BarLength    int    `long:"barlength" description:"Bar length of the chart" default:"60"`
-	BinBound     int    `long:"bound" description:"Upper bound of the chart"`
-	Help         bool   `short:"h" long:"help" description:"Show this help message"`
-}
-
-func parseTime(layout, text string, loc *time.Location) (time.Time, error) {
-	switch strings.ToLower(layout) {
-	case "unix":
-		f, err := strconv.ParseFloat(text, 64)
-		if err != nil {
-			return time.Time{}, fmt.Errorf("failed to parse epoch time: %s", text)
-		}
-		return time.UnixMicro(int64(f * 1000000)).In(loc), nil
-	case "unix.milli":
-		f, err := strconv.ParseFloat(text, 64)
-		if err != nil {
-			return time.Time{}, fmt.Errorf("failed to parse epoch time: %s", text)
-		}
-		return time.UnixMicro(int64(f * 1000)).In(loc), nil
-	case "unix.micro":
-		f, err := strconv.ParseFloat(text, 64)
-		if err != nil {
-			return time.Time{}, fmt.Errorf("failed to parse epoch time: %s", text)
-		}
-		return time.UnixMicro(int64(f)).In(loc), nil
-	case "ansic":
-		return time.ParseInLocation(time.ANSIC, text, loc)
-	case "unixdate":
-		return time.ParseInLocation(time.UnixDate, text, loc)
-	case "rubydate":
-		return time.ParseInLocation(time.RubyDate, text, loc)
-	case "rfc822":
-		return time.ParseInLocation(time.RFC822, text, loc)
-	case "rfc822z":
-		return time.ParseInLocation(time.RFC822Z, text, loc)
-	case "rfc850":
-		return time.ParseInLocation(time.RFC850, text, loc)
-	case "rfc1123":
-		return time.ParseInLocation(time.RFC1123, text, loc)
-	case "rfc1123z":
-		return time.ParseInLocation(time.RFC1123Z, text, loc)
-	case "rfc3339":
-		return time.ParseInLocation(time.RFC3339, text, loc)
-	case "rfc3339nano":
-		return time.ParseInLocation(time.RFC3339Nano, text, loc)
-	case "kitchen":
-		return time.ParseInLocation(time.Kitchen, text, loc)
-	case "stamp":
-		return time.ParseInLocation(time.Stamp, text, loc)
-	case "stampmilli":
-		return time.ParseInLocation(time.StampMilli, text, loc)
-	case "stampmicro":
-		return time.ParseInLocation(time.StampMicro, text, loc)
-	case "stampnano":
-		return time.ParseInLocation(time.StampNano, text, loc)
-	case "datetime":
-		return time.ParseInLocation(time.DateTime, text, loc)
-	case "dateonly":
-		return time.ParseInLocation(time.DateOnly, text, loc)
-	case "timeonly":
-		return time.ParseInLocation(time.TimeOnly, text, loc)
-	default:
-		return time.ParseInLocation(layout, text, loc)
-	}
-}
-
-func parseOpts(params []string) (*options, []string, error) {
-	var opts options
-
-	fp := flags.NewParser(&opts, flags.Default&^flags.HelpFlag)
-	fp.Usage = "[Options]"
-
-	args, err := fp.ParseArgs(params)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	if opts.Help {
-		var message bytes.Buffer
-
-		fp.WriteHelp(&message)
-		fmt.Fprint(&message, `
-Format Examples:
+const helpText = `Format Examples:
     ANSIC       "Mon Jan _2 15:04:05 2006"
     UnixDate    "Mon Jan _2 15:04:05 MST 2006"
     RubyDate    "Mon Jan 02 15:04:05 -0700 2006"
@@ -126,11 +39,111 @@ Format Examples:
     Unix-Milli  "1136239445000"
     Unix-Micro  "1136239445000000"
 
-    Arbitrary formats are also supported. See https://pkg.go.dev/time as a reference.`)
+    Arbitrary formats are also supported. See https://pkg.go.dev/time as a reference.`
 
-		fmt.Println(message.String())
+var layouts = map[string]string{
+	"ansic":       time.ANSIC,
+	"unixdate":    time.UnixDate,
+	"rubydate":    time.RubyDate,
+	"rfc822":      time.RFC822,
+	"rfc822z":     time.RFC822Z,
+	"rfc850":      time.RFC850,
+	"rfc1123":     time.RFC1123,
+	"rfc1123z":    time.RFC1123Z,
+	"rfc3339":     time.RFC3339,
+	"rfc3339nano": time.RFC3339Nano,
+	"kitchen":     time.Kitchen,
+	"stamp":       time.Stamp,
+	"stampmilli":  time.StampMilli,
+	"stampmicro":  time.StampMicro,
+	"stampnano":   time.StampNano,
+	"datetime":    time.DateTime,
+	"dateonly":    time.DateOnly,
+	"timeonly":    time.TimeOnly,
+}
+
+var epochLayouts = map[string]int64{
+	"unix":       1e6,
+	"unix-milli": 1e3,
+	"unix-micro": 1,
+}
+
+type options struct {
+	Format       string `short:"f" long:"format" description:"Time format for parsing the input text (default: guess)"`
+	TimeInterval string `short:"i" long:"interval" description:"Time duration for aggregation" default:"5m"`
+	TimeFrom     string `long:"time-from" description:"Time to display the chart from"`
+	TimeTo       string `long:"time-to" description:"Time to display the chart to"`
+	TimeZone     string `long:"time-zone" description:"TimeZone to display the time" default:"UTC"`
+	BarLength    int    `long:"barlength" description:"Bar length of the chart" default:"60"`
+	BinBound     int    `long:"bound" description:"Upper bound of the chart"`
+	Help         bool   `short:"h" long:"help" description:"Show this help message"`
+}
+
+type guessRule struct {
+	re      *regexp.Regexp
+	layouts []string
+}
+
+var guessRules = []guessRule{
+	{regexp.MustCompile(`^\d{10,19}(?:\.\d+)?$`), []string{"unix", "unix-milli", "unix-micro"}},
+	{regexp.MustCompile(`^\d{4}`), []string{"rfc3339", "rfc3339nano", "datetime", "dateonly"}},
+	{regexp.MustCompile(`[A-Za-z]{3,4}|[+-]\d{4}`), []string{"unixdate", "rubydate", "rfc822", "rfc822z", "rfc850", "rfc1123", "rfc1123z", "rfc3339", "rfc3339nano"}},
+	{regexp.MustCompile(`^[A-Za-z]{3},?`), []string{"ansic", "unixdate", "rubydate", "rfc822", "rfc822z", "rfc850", "rfc1123", "rfc1123z", "stamp", "stampmilli", "stampmicro", "stampnano"}},
+	{regexp.MustCompile(`\d{2}:\d{2}:\d{2}`), []string{"datetime", "timeonly", "ansic", "unixdate", "rubydate", "rfc850", "rfc1123", "rfc1123z"}},
+	{regexp.MustCompile(`\d{1,2}:\d{2}(AM|PM)`), []string{"kitchen"}},
+}
+
+func stringToTime(s, format string) (time.Time, error) {
+	if format == "" {
+		return guessTime(s)
+	}
+
+	if layout, ok := layouts[format]; ok {
+		return time.Parse(layout, s)
+	}
+
+	if scale, ok := epochLayouts[strings.ToLower(format)]; ok {
+		v, err := strconv.ParseFloat(s, 64)
+		if err != nil {
+			return time.Time{}, fmt.Errorf("failed to parse epoch time: %s", s)
+		}
+		return time.UnixMicro(int64(v * float64(scale))), nil
+	}
+
+	return time.Time{}, fmt.Errorf("failed to parse time: %s", s)
+}
+
+func guessTime(s string) (time.Time, error) {
+	for _, rule := range guessRules {
+		if rule.re.MatchString(s) {
+			for _, l := range rule.layouts {
+				if t, err := stringToTime(s, l); err == nil {
+					return t, nil
+				}
+			}
+		}
+	}
+	return time.Time{}, fmt.Errorf("Unknown format: %s", s)
+}
+
+func parseOpts(params []string) (*options, []string, error) {
+	var opts options
+	parser := flags.NewParser(&opts, flags.Default&^flags.HelpFlag)
+	parser.Usage = "[Options]"
+	args, err := parser.ParseArgs(params)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	if opts.Help {
+		var message bytes.Buffer
+		parser.WriteHelp(&message)
+		fmt.Fprint(&message, "\n")
+		fmt.Fprint(&message, helpText)
+		fmt.Fprintln(os.Stdout, message.String())
 		os.Exit(0)
 	}
+
 	return &opts, args, nil
 }
 
@@ -150,22 +163,24 @@ func run() error {
 		return err
 	}
 
+	format := strings.ToLower(opts.Format)
+
 	var timeFrom time.Time
 	if opts.TimeFrom != "" {
-		t, err := parseTime(opts.TimeLayout, opts.TimeFrom, loc)
+		t, err := stringToTime(opts.TimeFrom, format)
 		if err != nil {
 			return err
 		}
-		timeFrom = t.Truncate(gap)
+		timeFrom = t.In(loc).Truncate(gap)
 	}
 
 	var timeTo time.Time
 	if opts.TimeTo != "" {
-		t, err := parseTime(opts.TimeLayout, opts.TimeTo, loc)
+		t, err := stringToTime(opts.TimeTo, format)
 		if err != nil {
 			return err
 		}
-		timeTo = t.Truncate(gap).Add(gap)
+		timeTo = t.In(loc).Truncate(gap).Add(gap)
 	}
 
 	readers := make([]io.Reader, 0)
@@ -188,12 +203,12 @@ func run() error {
 	scanner := bufio.NewScanner(io.MultiReader(readers...))
 	for scanner.Scan() {
 		line := strings.TrimSpace(scanner.Text())
-		t, err := parseTime(opts.TimeLayout, line, loc)
+		t, err := stringToTime(line, format)
 		if err != nil {
 			continue
 		}
 
-		items = append(items, t)
+		items = append(items, t.In(loc))
 	}
 	if err := scanner.Err(); err != nil {
 		return err
